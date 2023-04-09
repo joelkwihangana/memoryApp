819df458dda54a223138f1ef1842566817fe0a85
Joel Kwihangana <joelkwijoe@gmail.com>
2023-04-10 00:39:34 +0200

    hiding my CONNECTION_STRING and PORT into .env for privacy

--- server/index.js
+++ server/index.js
@@ -3,19 +3,21 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://admin:FsqtNZnF43uoGbbo@cluster0.6icrjfa.mongodb.net/?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//   "mongodb+srv://admin:FsqtNZnF43uoGbbo@cluster0.6icrjfa.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
